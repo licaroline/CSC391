3b8f59094f266db7d501386c9ef856cd375d8076
licaroline <licarolinem1@gmail.com>
2019-02-06 16:02:03 -0500

    frequency analysis updated

--- Project1/4_frequency_analysis.py
+++ Project1/4_frequency_analysis.py
@@ -16,6 +16,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import skimage as ski
 
+file = 'DSC_9259'   # name of file without extension
+
 def plot3d(f2, img):
     """
     Visualizes the magnitude of the Fourier coefficients from the 2D 
@@ -36,7 +38,12 @@ def plot3d(f2, img):
     ax.plot_surface(X, Y, np.fft.fftshift(np.abs(f2)), cmap=plt.get_cmap('coolwarm'), antialiased=False)
 
     plt.show()
-    fig.savefig('dft_3d.png')
+    fig.savefig(file + '_3d.jpg')
+
+    # plot where y = 0
+    # plt.plot(X, np.fft.fftshift(np.abs(f2)))
+    # plt.xlim(-40, 40)
+    # plt.show()
 
 def plot2d(f2):
     """
@@ -54,12 +61,29 @@ def plot2d(f2):
     cv2.imshow('Magnitude Plot', magnitude_img)
     cv2.waitKey(0)
 
-    cv2.imwrite('dft_2d.png', magnitude_img)
+    cv2.imwrite(file + '_2d.jpg', magnitude_img)
 
-def plotlog(f2, img):
+def plotlog(f2):
     """
     Visualizes the log of the magnitude of the Fourier coefficients 
     from the 2D DFT of an image as a 2D plot.
+
+    Args:
+        f2 (ndarray, complex128): 2D DFT of an image
+    """
+    magnitude_img = np.fft.fftshift(np.log(np.abs(f2)))
+    magnitude_img = magnitude_img / magnitude_img.max()
+    magnitude_img = ski.img_as_ubyte(magnitude_img)
+
+    cv2.imshow('Magnitude Plot', magnitude_img)
+    cv2.waitKey(0)
+
+    cv2.imwrite(file + '_log.jpg', magnitude_img)
+
+def plotlog3d(f2, img):
+    """
+    Visualizes the log of the magnitude of the Fourier coefficients 
+    from the 2D DFT of an image as a 3D plot.
 
     Args:
         f2 (ndarray, complex128): 2D DFT of an image
@@ -76,11 +100,71 @@ def plotlog(f2, img):
     ax.plot_surface(X, Y, np.fft.fftshift(np.log(np.abs(f2))), cmap=plt.get_cmap('coolwarm'), antialiased=False)
 
     plt.show()
-    fig.savefig('dft_log.png')
+    fig.savefig(file + '_log3d.jpg')
+
+def zerolow(f2, img):
+    """
+    Takes an image and its 2D DFT and zeroes out the lowest 0.1% of 
+    coefficients and displays the results.
+
+    Args:
+        f2 (ndarray, complex128): 2D DFT of an image
+        img (ndarray, uint8): the original image
+    """
+
+    # get lowest 0.1% of coefficients and zero out
+    low_values_flags = np.abs(f2) < ((f2.max() - f2.min()) / 1000)
+    f2[low_values_flags] = 0
+
+    plot3d(f2, img)
+
+    # do inverse 2D DFT
+    new_img = np.fft.ifft2(f2)
+    new_img = new_img / new_img.max()
+
+    greater_flag = new_img > 1
+    new_img[greater_flag] = 1
+    lesser_flag = new_img < -1
+    new_img[lesser_flag] = -1
+
+    new_img = ski.img_as_ubyte(new_img.real)
+
+    cv2.imshow('Original Image', img)
+    cv2.imshow('New Image', new_img)
+    cv2.waitKey(0)
+    
+def zerohigh(f2, img):
+    """
+    Takes an image and its 2D DFT and zeroes out the highest 0.1% of 
+    coefficients and displays the results.
+
+    Args:
+        f2 (ndarray, complex128): 2D DFT of an image
+        img (ndarray, uint8): the original image
+    """
+
+    # get highest 0.1% of coefficients and zero out
+    high_values_flags = np.abs(f2) > (f2.max() - ((f2.max() - f2.min()) / 1000))
+    f2[high_values_flags] = 0
+
+    # do inverse 2D DFT
+    new_img = np.fft.ifft2(f2)
+    new_img = new_img / new_img.max()
+
+    greater_flag = new_img > 1
+    new_img[greater_flag] = 1
+    lesser_flag = new_img < -1
+    new_img[lesser_flag] = -1
+
+    new_img = ski.img_as_ubyte(new_img.real)
+
+    cv2.imshow('Original Image', img)
+    cv2.imshow('New Image', new_img)
+    cv2.waitKey(0)
 
 if __name__ == "__main__":
     dir = os.path.dirname(__file__)
-    filename = os.path.join(dir, 'Images/DSC_9259.JPG') # change name of file accordingly
+    filename = os.path.join(dir, 'Images/' + file + '.JPG') # change name of file accordingly
 
     if not os.path.exists(filename):
         print("File does not exist.")
@@ -98,4 +182,8 @@ if __name__ == "__main__":
     plot2d(f2)
 
     # plot log of magnitude + 1
-    plotlog(f2, img)
+    plotlog(f2)
+
+    # plot log of magnitude + 1 in 3D
+    plotlog3d(f2, img)
+    
